56f4e51ed300201aed3988e166575f5bf58de38e
Bariqa-Anwar <bariqaanwar34@gmail.com>
2025-12-19 15:08:00 +0500

    added cors

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -13,7 +13,7 @@ export default function Root({children}) {
     setInput('');
 
     try {
-      const response = await fetch('http://127.0.0.1:8080/chat', {
+      const response = await fetch('https://Bariqa-Humanoid-Robotics.hf.space/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: input }),
